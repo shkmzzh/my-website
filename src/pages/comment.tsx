f01837d548afa7754383cab00c4301c6b92308ea
xiao <87223862+shkmzzh@users.noreply.github.com>
2024-08-29 18:44:47 +0800

    feat

--- src/pages/comment.tsx
+++ src/pages/comment.tsx
@@ -1,8 +1,35 @@
 import Layout from '@/components/layout';
 import { ReactElement } from 'react';
+import Card from '@/components/card';
+import Giscus from '@giscus/react';
 
 export default function Comment() {
-  return <div className={`flex`}>comment</div>;
+  return (
+    <main className='py-5 md:py-header"'>
+      <Card>
+        <article className="p-4 md:p-6">
+          <header className="mb-10 ">
+            <h1 className="text-2xl md:text-3xl ">评论留言</h1>
+          </header>
+          <Giscus
+            id="comments"
+            repo="shkmzzh/Discussion"
+            repoId="R_kgDOMnKBsg"
+            category="Announcements"
+            categoryId="DIC_kwDOMnKBss4Ch3mF"
+            mapping="pathname"
+            term="Welcome to @giscus/react component!"
+            reactionsEnabled="1"
+            emitMetadata="0"
+            inputPosition="top"
+            theme="light"
+            lang="zh-CN"
+            loading="lazy"
+          />
+        </article>
+      </Card>
+    </main>
+  );
 }
 
 Comment.getLayout = function getLayout(page: ReactElement) {
