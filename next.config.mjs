bf68adaa607f3d2139a7d940d391f424e54e3cab
xiao <87223862+shkmzzh@users.noreply.github.com>
2025-03-14 17:56:16 +0800

    feat

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,12 @@
 /** @type {import('next').NextConfig} */
-// 用于为静态资源（如图像、样式表、JavaScript 文件等）设置 URL 前缀
-// 这在将应用部署到自定义域名或 CDN 上时特别有用，因为它允许您将静态资源存储在不同的位置
-let assetPrefix = ``
-// 用于为应用设置基础路径。这在将应用部署到子目录下时特别有用，因为它允许您指定应用所在的目录
-let basePath = `/`
+let assetPrefix = ''
+let basePath = '/'
 
-// const isGithubPage = process.env.NODE_ENV === 'gh'
-const isGithubActions = process.env.GITHUB_ACTIONS || false
+const isGithubActions = Boolean(process.env.GITHUB_ACTIONS)
 
-if (isGithubActions) {
+if (isGithubActions && process.env.GITHUB_REPOSITORY) {
   const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
-  assetPrefix = `/${repo}/`
+  assetPrefix = `/${repo}`
   basePath = `/${repo}`
 }
 
@@ -19,8 +15,8 @@ const nextConfig = {
   basePath,
   reactStrictMode: true,
   images: {
-    unoptimized: true,
+    unoptimized: isGithubActions, // 仅在 GitHub Actions 下关闭图片优化
   },
 }
 
-export default nextConfig;
+export default nextConfig
