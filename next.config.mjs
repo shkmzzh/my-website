efbaeaf1c73d84ffd413ee5b5cd885c68d71e693
xiao <87223862+shkmzzh@users.noreply.github.com>
2025-03-14 18:16:51 +0800

    feat

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,6 @@
 /** @type {import('next').NextConfig} */
-let assetPrefix = ''
-let basePath = '/'
-
-const isGithubActions = Boolean(process.env.GITHUB_ACTIONS)
-
-if (isGithubActions && process.env.GITHUB_REPOSITORY) {
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
-  assetPrefix = `/${repo}`
-  basePath = `/${repo}`
-}
-
 const nextConfig = {
-  assetPrefix,
-  basePath,
   reactStrictMode: true,
-  images: {
-    unoptimized: isGithubActions, // 仅在 GitHub Actions 下关闭图片优化
-  },
-}
+};
 
-export default nextConfig
+export default nextConfig;
