6184bb7facda428172098dffcde843ae5440bd1e
xiao <87223862+shkmzzh@users.noreply.github.com>
2025-03-17 15:13:53 +0800

    fix

--- src/pages/moment.tsx
+++ src/pages/moment.tsx
@@ -2,7 +2,7 @@ import Layout from '@/components/layout';
 import { ReactElement } from 'react';
 
 export default function Moment() {
-  return <div className={`flex`}>mmhhdsda1</div>;
+  return <div className={`flex`}>mm</div>;
 }
 
 Moment.getLayout = function getLayout(page: ReactElement) {
