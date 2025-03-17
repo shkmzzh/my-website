d0308db5ba910616d4a01b8aee0fae41525933f7
xiao <87223862+shkmzzh@users.noreply.github.com>
2025-03-17 11:17:48 +0800

    feat

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  output:'export'
+  output: 'export',  // 开启静态导出
+  // 如果部署在子路径下，也可配置 basePath 和 assetPrefix
 };
 
-export default nextConfig;
+module.exports = nextConfig;
