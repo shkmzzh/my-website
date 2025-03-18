db1e58794a4f2bf47639231ddbec206b80f8704d
xiao <87223862+shkmzzh@users.noreply.github.com>
2025-03-18 16:44:23 +0800

    feat

--- src/pages/moment.tsx
+++ src/pages/moment.tsx
@@ -2,7 +2,7 @@ import Layout from '@/components/layout';
 import { ReactElement } from 'react';
 
 export default function Moment() {
-  return <div className={`flex`}>我叫牛马怀，是个老瓢虫8🐞</div>;
+  return <div className={`flex`}>我叫牛马怀，是个老瓢虫8🐞1</div>;
 }
 
 Moment.getLayout = function getLayout(page: ReactElement) {
