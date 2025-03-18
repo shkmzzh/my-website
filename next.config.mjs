8606f8a2a1c97c5a5f41bcbcfa949bcc7a39bb21
xiao <87223862+shkmzzh@users.noreply.github.com>
2025-03-18 11:01:31 +0800

    feat

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,8 @@
 /** @type {import('next').NextConfig} */
-const isGithubActions = process.env.GITHUB_ACTIONS === 'true';
 
-let basePath = '';
-let assetPrefix = '';
-
-if (isGithubActions && process.env.GITHUB_REPOSITORY) {
-  // 从 "owner/repo" 中取出 repo 名称
-  const repo = process.env.GITHUB_REPOSITORY.split('/')[1];
-  basePath = `/${repo}`;
-  assetPrefix = `/${repo}/`;
-}
 
 const nextConfig = {
-  output: 'export', // 开启静态导出功能
-  reactStrictMode: true,
-  // 如果有需要，还可以配置 images.unoptimized 为 true
-  compress:true,
+  output: 'export',
 };
 
 export default nextConfig;
