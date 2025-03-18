8dc95fee03e3b38bd561d281c178663217f76fc6
xiao <87223862+shkmzzh@users.noreply.github.com>
2025-03-18 17:09:22 +0800

    feat

--- next.config.mjs
+++ next.config.mjs
@@ -1,26 +1,7 @@
 /** @type {import('next').NextConfig} */
-const isGithubActions = process.env.GITHUB_ACTIONS === 'true';
-
-let basePath = '';
-let assetPrefix = '';
-
-if (isGithubActions && process.env.GITHUB_REPOSITORY) {
-  const repo = process.env.GITHUB_REPOSITORY.split('/')[1];
-  basePath = `/${repo}`;
-  assetPrefix = `/${repo}/`;
-}
 
 const nextConfig = {
-  output: 'export',
-  reactStrictMode: true,
-  basePath,
-  assetPrefix,
-  images: {
-    unoptimized: true,
-  },
-  publicRuntimeConfig: {
-    basePath,
-  },
+  output: 'export'
 };
 
 export default nextConfig;
